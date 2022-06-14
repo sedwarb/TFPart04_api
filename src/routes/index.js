631e2531f4aebe8956339bb4c70074fa8a622ffa
carmarfuc <mariano.fuchilieri@gmail.com>
2022-06-14 20:23:53 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,13 +9,13 @@ const Category = require('./category');
 const Order = require('./order.js');
 const MercadoPago = require('./mercadopago.js');
 const Password = require('./password.js');
-const Review2 = require('./review.js');
+const Review = require('./review.js');
 
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use('/review', Review2);
+router.use('/review', Review);
 router.use('/product', Product);
 router.use('/category', Category);
 router.use('/user', User);
